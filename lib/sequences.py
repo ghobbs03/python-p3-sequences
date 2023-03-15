5be89c56a3cfb36884fd914f13726411b3291a03
Gail Hobbs <gbhobbs97@gmail.com>
2023-03-14 21:07:11 -0700

    completed

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if (length == 0):
+        print([])
+        return
+
+    array = [None]*(length+1)
+    array[0] = 0 #first
+    array[1] = 1 #second
+
+    for i in range(2, length+1):
+        array[i] = array[i-1] + array[i-2] # 1 + 1
+        
+    print(array[:length])
